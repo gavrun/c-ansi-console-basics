302176c3ffb2bf9cc57d7f7907401e61b20552b2
gavrun <38539655+gavrun@users.noreply.github.com>
2025-10-07 23:40:12 +0100

    Created project template for C

---
+++ ConsoleApp/ConsoleApp.c
@@ -0,0 +1,25 @@
+// ConsoleApp.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+//
+// NOTE: Visual Studio does not provide a built-in "Console App (C)" template.
+// 
+// Create a "Console App (C++)" or "Empty Project", then:
+//   1. Add a source file with the .c extension
+//   2. Set project to "Compile As C Code (/TC)"
+//   3. Set project to "Legacy MSVC (C89 or ANSI C)"
+//
+// This project uses ANSI C (C89/C90) with elements compatible with C99.
+// For input/output in C use <stdio.h>.
+//
+// Save a source file with character encoding UTF-8 with BOM for Cyrillic support
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
